c543ed114915858921b13ddcf6990924e7516fed
nogsposito <vsan@cesar.school>
2024-12-03 20:49:29 -0300

    alteracoes

--- src/main/java/com/vini_first_spring/first_spring_app/FirstSpringAppApplication.java
+++ src/main/java/com/vini_first_spring/first_spring_app/FirstSpringAppApplication.java
@@ -2,8 +2,10 @@ package com.vini_first_spring.first_spring_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication // combina Configuration, EnableAutoConfigurarion e ComponentScan
+@Profile("dev")
 public class FirstSpringAppApplication {
 
 	public static void main(String[] args) { // classe pprincipal

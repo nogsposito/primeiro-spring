255ed04095b0b402624c3929909f3e58a3f5bccb
nogsposito <vsan@cesar.school>
2024-12-03 21:37:06 -0300

    alteracoes

--- src/main/java/com/vini_first_spring/first_spring_app/controller/HelloWorldController.java
+++ src/main/java/com/vini_first_spring/first_spring_app/controller/HelloWorldController.java
@@ -2,9 +2,14 @@ package com.vini_first_spring.first_spring_app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vini_first_spring.first_spring_app.domain.User;
 import com.vini_first_spring.first_spring_app.service.HelloWorldService;
 
 @RestController //@Controller e @ResponseBody (está criando api liadando com back-end apenas)
@@ -15,12 +20,13 @@ public class HelloWorldController {
     @Autowired // ja faz injecao de dependencia de forma automatica
     private HelloWorldService helloWorldService;
     
-    //public HelloWorldController(HelloWorldService helloWorldService){
-    //    this.helloWorldService = helloWorldService;
-    //}
-    
     @GetMapping // responde requisicoes geet do /hello-world
     public String helloWorld(){
         return helloWorldService.helloWorld("vini");
+    }
+
+    @PostMapping("/{id}") // requisicao de post (manda)
+    public String helloWorldPost(@PathVariable("id") String id, @RequestParam(value = "filter", defaultValue = "nenhum") String filter, @RequestBody User body){ // parametros tambem podem receber decoracoes! injeta no body o que vem da requisicao
+        return "Hello World " + filter;
     }
 }
